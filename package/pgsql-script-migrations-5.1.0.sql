5bd5a916622f7963d7cb7161d2e72399292fbd52
Luca Gasparini <luca.gasparini@zextras.com>
2022-11-17 13:19:53 +0100

    [CHATS-420] Fixed script for DB migration

--- package/pgsql-script-migrations-5.1.0.sql
+++ package/pgsql-script-migrations-5.1.0.sql
@@ -3,4 +3,4 @@ CREATE TABLE chats_messaging_db_version
     version VARCHAR(16) NOT NULL,
     PRIMARY KEY (version)
 );
-INSERT INTO carbonio_chats_messaging_db_version (version) VALUES ('5.1.0');
+INSERT INTO chats_messaging_db_version (version) VALUES ('5.1.0');
