1c133381912adf2a88f31c872f09b3ba35982344
Perla Polito <perla.polito@zextras.com>
2023-01-20 11:56:02 +0100

    chore: [CHATS-541] Add license to every file of the project

--- package/pgsql-script-initial-schema-mongooseim-5.1.0.sql
+++ package/pgsql-script-initial-schema-mongooseim-5.1.0.sql
@@ -1,20 +1,7 @@
+-- Copyright (C) 2002-2011   ProcessOne
+-- SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
 --
--- ejabberd, Copyright (C) 2002-2011   ProcessOne
---
--- This program is free software; you can redistribute it and/or
--- modify it under the terms of the GNU General Public License as
--- published by the Free Software Foundation; either version 2 of the
--- License, or (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
--- General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
---
+-- SPDX-License-Identifier: AGPL-3.0-only
 
 CREATE TYPE test_enum_char AS ENUM('A','B', 'C');
 CREATE TABLE test_types(
