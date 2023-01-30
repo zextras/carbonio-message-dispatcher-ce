a70aa353b8ba25b083cf874c07fb3f7ebf101788
Perla Polito <perla.polito@zextras.com>
2023-01-30 12:37:51 +0100

    chore: [CHATS-541] Add license to pgsql-script-migrations-6.0.0.sql

--- package/pgsql-script-migrations-6.0.0.sql
+++ package/pgsql-script-migrations-6.0.0.sql
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+--
+-- SPDX-License-Identifier: AGPL-3.0-only
+
 -- DOMAINS
 ALTER TABLE domain_settings
     ALTER COLUMN enabled DROP DEFAULT;
