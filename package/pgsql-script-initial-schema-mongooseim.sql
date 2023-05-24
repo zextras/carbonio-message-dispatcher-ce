e45ef3199bdd786934da82477a6f8de699558b19
Noman <noman.alishaukat@zextras.com>
2023-05-24 17:22:31 +0200

    chore: refactor pgsql files management

--- package/pgsql-script-initial-schema-mongooseim.sql
+++ package/pgsql-script-initial-schema-mongooseim.sql
@@ -1,7 +1,20 @@
--- Copyright (C) 2002-2011   ProcessOne
--- SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
 --
--- SPDX-License-Identifier: AGPL-3.0-only
+-- ejabberd, Copyright (C) 2002-2011   ProcessOne
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation; either version 2 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+--
 
 CREATE TYPE test_enum_char AS ENUM('A','B', 'C');
 CREATE TABLE test_types(
@@ -477,7 +490,7 @@ CREATE TABLE domain_settings (
     id BIGSERIAL NOT NULL UNIQUE,
     domain VARCHAR(250) NOT NULL,
     host_type VARCHAR(250) NOT NULL,
-    enabled BOOLEAN NOT NULL DEFAULT true,
+    status SMALLINT NOT NULL DEFAULT 1,
     PRIMARY KEY(domain)
 );
 
--- package/pgsql-script-migrations-6.0.0.sql
+++ package/pgsql-script-migrations-6.0.0.sql
@@ -2,26 +2,9 @@
 --
 -- SPDX-License-Identifier: AGPL-3.0-only
 
--- DOMAINS
-ALTER TABLE domain_settings
-    ALTER COLUMN enabled DROP DEFAULT;
-
-ALTER TABLE domain_settings
-    ALTER COLUMN enabled TYPE SMALLINT USING CASE WHEN enabled THEN 1 ELSE 0 END;
-
-ALTER TABLE domain_settings
-    RENAME enabled TO status;
-
-ALTER TABLE domain_settings
-    ALTER COLUMN status SET DEFAULT 1;
-
--- Table containing domain_admins
--- They are being used by graphql_admin API
-CREATE TABLE domain_admins
+CREATE TABLE database_version
 (
-    domain       VARCHAR(250) NOT NULL,
-    pass_details TEXT         NOT NULL,
-    PRIMARY KEY (domain)
+    version VARCHAR(16) NOT NULL,
+    PRIMARY KEY (version)
 );
-
-UPDATE database_version SET version = '6.0.0';
+INSERT INTO database_version (version) VALUES ('6.0.0');
